9feb73508e4814fade8070a97e5c5fea7cbfd08d
jencia <1073956837@qq.com>
2021-02-16 15:38:08 +0800

    chore: update config

--- rollup.config.js
+++ rollup.config.js
@@ -18,95 +18,93 @@ const onwarn = (warning, onwarn) =>
     warning.message.includes('/@sapper/')) ||
   onwarn(warning)
 
-export default {
-  client: {
-    input: config.client.input(),
-    output: config.client.output(),
-    plugins: [
-      replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
-      svelte({
-        dev,
-        hydratable: true,
-        emitCss: true,
-      }),
-      resolve(),
-      commonjs(),
-      markdown(),
-      glob(),
-      legacy &&
-        babel({
-          extensions: ['.js', '.mjs', '.html', '.svelte'],
-          babelHelpers: 'runtime',
-          exclude: ['node_modules/@babel/**'],
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: '> 0.25%, not dead',
-              },
-            ],
+export const client = {
+  input: config.client.input(),
+  output: config.client.output(),
+  plugins: [
+    replace({
+      'process.browser': true,
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    }),
+    svelte({
+      dev,
+      hydratable: true,
+      emitCss: true,
+    }),
+    resolve(),
+    commonjs(),
+    markdown(),
+    glob(),
+    legacy &&
+      babel({
+        extensions: ['.js', '.mjs', '.html', '.svelte'],
+        babelHelpers: 'runtime',
+        exclude: ['node_modules/@babel/**'],
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: '> 0.25%, not dead',
+            },
           ],
-          plugins: [
-            '@babel/plugin-syntax-dynamic-import',
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                useESModules: true,
-              },
-            ],
+        ],
+        plugins: [
+          '@babel/plugin-syntax-dynamic-import',
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              useESModules: true,
+            },
           ],
-        }),
-
-      !dev &&
-        terser({
-          module: true,
-        }),
-    ],
-    preserveEntrySignatures: false,
-    onwarn,
-  },
-
-  server: {
-    input: config.server.input(),
-    output: config.server.output(),
-    plugins: [
-      replace({
-        'process.browser': false,
-        'process.env.NODE_ENV': JSON.stringify(mode),
+        ],
       }),
-      svelte({
-        generate: 'ssr',
-        dev,
+
+    !dev &&
+      terser({
+        module: true,
       }),
-      resolve(),
-      commonjs(),
-      markdown(),
-      glob(),
-    ],
-    external: Object.keys(pkg.dependencies).concat(
-      require('module').builtinModules ||
-        Object.keys(process.binding('natives'))
-    ),
+  ],
+  preserveEntrySignatures: false,
+  onwarn,
+}
 
-    onwarn,
-  },
+export const server = {
+  input: config.server.input(),
+  output: config.server.output(),
+  plugins: [
+    replace({
+      'process.browser': false,
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    }),
+    svelte({
+      generate: 'ssr',
+      dev,
+    }),
+    resolve(),
+    commonjs(),
+    markdown(),
+    glob(),
+  ],
+  external: Object.keys(pkg.dependencies).concat(
+    require('module').builtinModules ||
+      Object.keys(process.binding('natives'))
+  ),
 
-  serviceworker: {
-    input: config.serviceworker.input(),
-    output: config.serviceworker.output(),
-    plugins: [
-      resolve(),
-      replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
-      commonjs(),
-      !dev && terser(),
-    ],
+  onwarn,
+}
 
-    onwarn,
-  },
+export const serviceworker = {
+  input: config.serviceworker.input(),
+  output: config.serviceworker.output(),
+  plugins: [
+    resolve(),
+    replace({
+      'process.browser': true,
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    }),
+    commonjs(),
+    !dev && terser(),
+  ],
+
+  onwarn,
 }
